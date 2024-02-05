e9936eaa48ab62513e5b6efb4afd774dad13ce67
Jeremiah Goyette <jgoyette@knox-networks.com>
2024-02-05 15:31:55 -0600

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,8 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545, // default for Ganache
-      network_id: "4447"
+      port: 8545, // default for Ganache
+      network_id: "*"
     }
   }
 }
